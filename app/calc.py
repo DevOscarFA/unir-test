5f97a2c29d6d04d10c749aee2e704511305d29b0
DevOscarFA <oscarfdzalz@gmail.com>
2022-11-24 09:26:11 -0500

    cambios en repositorio

--- app/calc.py
+++ app/calc.py
@@ -1,10 +1,3 @@
-import app, math
-
-
-class InvalidPermissions(Exception):
-    pass
-
-
 class Calculator:
     def add(self, x, y):
         self.check_types(x, y)
@@ -15,72 +8,26 @@ class Calculator:
         return x - y
 
     def multiply(self, x, y):
-        if not app.util.validate_permissions(f"{x} * {y}", "user1"):
-            raise InvalidPermissions("El usuario no tiene permisos")
-
         self.check_types(x, y)
         return x * y
 
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise TypeError("La división por cero no es posible")
+            raise TypeError("Division by zero is not possible")
 
         return x / y
 
-    def remainder(self, x, y):
-        self.check_types(x, y)
-        if y == 0:
-            raise TypeError("La división por cero no es posible")
-        return x % y
-
     def power(self, x, y):
         self.check_types(x, y)
-        if x == 0 and y < 0:
-            raise TypeError("Cero no se puede elevar a una potencia negativa")
-        return x**y
+        return x ** y
 
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Los parámetros deben ser números.")
-
-    def sqrt(self, x):
-        self.check_types(x)
-
-        if x < 0:
-            raise TypeError(
-                "No se puede calcular la raíz cuadrada de un número negativo sin usar números complejos"
-            )
-
-        return math.sqrt(x)
-
-    def logarithm10(self, x):
-        self.check_types(x)
-
-        if x <= 0:
-            raise TypeError("No se puede calcular log10 de números negativos o cero")
-
-        return math.log10(x)
-
-    def check_types(self, x, y=0):
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Los parámetros deben ser números")
+            raise TypeError("Parameters must be numbers")
 
 
 if __name__ == "__main__":  # pragma: no cover
     calc = Calculator()
-    resultadd = calc.add(2, 2)
-    resultsubstract = calc.substract(2, 1)
-    resultdivide = calc.divide(8, 2)
-    resultpower = calc.power(2, 4)
-    resultremainder = calc.remainder(2, 1)
-    resultsquareroot = calc.sqrt(9)
-    resultlogarithm10 = calc.logarithm10(2)
-
-    print("resultadd: ", resultadd)
-    print("resultsubstract: ", resultsubstract)
-    print("resultdivide: ", resultdivide)
-    print("resultpower: ", resultpower)
-    print("resultremainder: ", resultremainder)
-    print("resultsquareroot: ", resultsquareroot)
-    print("resultlogarithm10: ", resultlogarithm10)
+    result = calc.add(2, 2)
+    print(result)
--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -16,63 +16,7 @@ class TestApi(unittest.TestCase):
         self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
 
     def test_api_add(self):
-        url = f"{BASE_URL}/calc/add/1/2"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-    
-    def test_api_substract(self):
-        url = f"{BASE_URL}/calc/substract/20/10"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-    
-    def test_api_multiply(self):
-        url = f"{BASE_URL}/calc/multiply/8/0"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-
-    def test_api_divide(self):
-        url = f"{BASE_URL}/calc/divide/2/2"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-    
-    def test_api_divide_0(self):
-        url = f"{BASE_URL}/calc/divide/2/0"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-
-    def test_api_power(self):
-        url = f"{BASE_URL}/calc/power/4/4"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-
-    def test_api_remainder(self):
-        url = f"{BASE_URL}/calc/remainder/4/4"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-
-    def test_api_squareroot(self):
-        url = f"{BASE_URL}/calc/sqrt/3"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-
-    def test_api_logarithm10(self):
-        url = f"{BASE_URL}/calc/logarithm10/2"
+        url = f"{BASE_URL}/calc/add/2/2"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
